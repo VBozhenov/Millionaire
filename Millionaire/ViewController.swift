b71fb49971e1381baf5d0047d38db0fc0daca1cc
Vladimir Bozhenov <ve.bozhenov@gmail.com>
2019-10-14 21:25:31 +0300

    создал проект

---
+++ Millionaire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Millionaire
+//
+//  Created by Vladimir Bozhenov on 14.10.2019.
+//  Copyright © 2019 Vladimir Bozhenov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
